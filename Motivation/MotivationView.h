e486cba49f996ee4f2b5832f47fe3ebe36c8c43c
rick <abneryaoo@gmail.com>
2023-10-09 10:15:32 +0800

    Initial Commit

---
+++ Motivation/MotivationView.h
@@ -0,0 +1,12 @@
+//
+//  MotivationView.h
+//  Motivation
+//
+//  Created by Rick Curly on 10/9/23.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MotivationView : ScreenSaverView
+
+@end
